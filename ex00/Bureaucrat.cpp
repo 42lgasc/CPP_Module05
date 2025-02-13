ea0e5eb04b54eed1b0e524bc7c5a1a9ea0fe66e2
lgasc <lgasc@student.42perpignan.fr>
2025-02-13 15:59:26 +0100

    Color "smartly" `Bureaucrat` messages
    Dear Sherpa, please forgive me sin.

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -6,17 +6,35 @@
 /*   By: lgasc <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 17:23:52 by lgasc             #+#    #+#             */
-/*   Updated: 2024/11/28 22:05:45 by lgasc            ###   ########.fr       */
+/*   Updated: 2025/02/13 15:51:19 by lgasc            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
+
+#if   __has_include ( "Form.hpp")
+# include             "Form.hpp"
+#elif __has_include ("AForm.hpp")
+# include            "AForm.hpp"
+#endif
+
+
+#if __has_include ("Form.hpp") or __has_include ("AForm.hpp") \
+	or defined DEBUG and DEBUG
+# include <iostream> // cout #+DEBUG clog, endl
+# include <unistd.h> // isatty, STDOUT_FILENO
+#endif
+
 #include <sstream> // ostringstream
 
-#if __has_include ("Form.hpp")
-# include "Form.hpp"
-# include <iostream> // cout
+#if defined DEBUG and DEBUG
+# define	   LOG	 "\x1B[7;90m"
+# define	NO_LOG	"\x1B[27;39m"
 #endif
+
+#define	CSI	"\x1B["
+
+
 
 static std::string
 ft_concatenate(const std::string & prefix, const unsigned char & suffix)
@@ -48,37 +66,38 @@ std::ostream	& operator << (std::ostream	& s, const Bureaucrat	& b)
 // Constants
 
 const Bureaucrat::Grade
-Bureaucrat::c_highest_grade (1),	Bureaucrat::c_lowest_grade (150);
+	Bureaucrat::c_highest_grade (1),	Bureaucrat::c_lowest_grade (150);
 
 
 // Canonical Four
 
-Bureaucrat::Bureaucrat		(void)							throw ():
-	name ("bureaucratic Bureaucrative"),	grade (c_lowest_grade)		{}
-Bureaucrat::Bureaucrat		(const Bureaucrat	& original)	throw ():
-	name (original.name), grade (original.grade)						{}
-Bureaucrat	& Bureaucrat::operator = (const Bureaucrat	& other) throw ()
+Bureaucrat::Bureaucrat  	(void)                          throw ():
+	name ("bureaucratic Bureaucrative"),	grade (c_lowest_grade)   {}
+Bureaucrat::Bureaucrat  	(const Bureaucrat	& original) throw ():
+	name (original.name), grade (original.grade)                     {}
+Bureaucrat              	& Bureaucrat::operator =
+	(const Bureaucrat	& other)                            throw ()
 { grade = other.grade; return *this; }
-Bureaucrat::~ Bureaucrat	(void)							throw ()	{}
+Bureaucrat::~ Bureaucrat	(void)                          throw () {}
 
 
 // other Constructors
 
 Bureaucrat::Bureaucrat
-(const std::string	& new_name, const unsigned char	& new_grade)
+	(const std::string	& new_name, const unsigned char	& new_grade)
 	throw (GradeTooHighException, GradeTooLowException):
 		name (new_name), grade (new_grade) {
-	if (grade < c_lowest_grade)		throw GradeTooLowException	();
-	if (grade > c_highest_grade)	throw GradeTooHighException	();
+	if (grade < c_lowest_grade)  throw GradeTooLowException  ();
+	if (grade > c_highest_grade) throw GradeTooHighException ();
 }
 
 
 // Accessors
 
 const std::string
-& Bureaucrat::getName(void)		const throw () { return name; }
+	& Bureaucrat::getName(void)  const throw () { return name;  }
 const Bureaucrat::Grade
-& Bureaucrat::getGrade(void)	const throw () { return grade; }
+	& Bureaucrat::getGrade(void) const throw () { return grade; }
 
 
 // Mutators
@@ -108,24 +127,41 @@ Bureaucrat	Bureaucrat::operator -- (int) throw (GradeTooLowException) {
 
 
 // Methods
+#if __has_include ("Form.hpp") or __has_include ("AForm.hpp")
 
-#if __has_include ("Form.hpp")
+# if   __has_include ( "Form.hpp")
+void	Bureaucrat::signForm( Form	& f) const throw () {
+# elif __has_include ("AForm.hpp")
+void	Bureaucrat::signForm(AForm	& f) const throw () {
+# endif
 
-void	Bureaucrat::signForm(Form	& f) const throw () {
-	const std::string	c_fail_message1 (" couldn't sign "),
-						c_fail_message2 (" because ");
+# if defined DEBUG and DEBUG
+	std::clog << LOG << __PRETTY_FUNCTION__ << '\t' << f.hasSignature()
+		<< NO_LOG << std::endl;
+# endif
+
+	const std::string	c_fail_message [] = {" couldn't sign ", " because "};
+
+	// Playin' with fire over here...
+	const char	(*const & prefix) [6]
+		= reinterpret_cast <const char (*) [6]>(& CSI"33m\0" CSI"32m\0" CSI"31m"
+	          	                             [not isatty(STDOUT_FILENO) * 5]),
+	          	*const & suffix = & CSI"39m" [not isatty(STDOUT_FILENO) * 5];
+
 	if (f.hasSignature()) {
-		std::cout << name << c_fail_message1 << f.getName() << c_fail_message2
-			<< "it has a signature already.\n";
+		std::cout << prefix [0] << name << c_fail_message [0] << f.getName()
+			<< c_fail_message [1] << "it has a signature already.\n" << suffix;
 		return ;
 	}
+
 	try {
-		f.beSigned(*this); std::cout << name << " signed " << f.getName()
-			<< '\n';
+		f.beSigned(*this);
+		std::cout << prefix [1] << name << " signed " << f.getName()
+			<< suffix << '\n';
 	}
 	catch (const std::exception	& e) {
-		std::cout << name << c_fail_message1 << f.getName() << c_fail_message2
-			<< e.what() << ".\n";
+		std::cout << prefix [2] << name << c_fail_message [0] << f.getName()
+			<< c_fail_message [1] << e.what() << ".\n" << suffix;
 	}
 }
 #endif
@@ -151,14 +187,16 @@ const std::string	Bureaucrat::GradeTooHighException::what
 
 // Canonical Four
 
-Bureaucrat::GradeTooHighException::GradeTooHighException	(void)
-							throw (): std::domain_error (what)				{}
+Bureaucrat::GradeTooHighException::GradeTooHighException	(void) throw ():
+	std::domain_error (what)                                                {}
 Bureaucrat::GradeTooHighException::GradeTooHighException
-(const t_self	& original)	throw (): std::domain_error (original)			{}
+	(const t_self	& original)                                    throw ():
+	std::domain_error (original)                                            {}
 Bureaucrat::GradeTooHighException
-& Bureaucrat::GradeTooHighException::operator = (const t_self & other) throw ()
+	& Bureaucrat::GradeTooHighException::operator =
+	(const t_self & other)                                         throw ()
 { std::domain_error::operator = (other); return *this; }
-Bureaucrat::GradeTooHighException::~ GradeTooHighException	(void) throw ()	{}
+Bureaucrat::GradeTooHighException::~ GradeTooHighException	(void) throw () {}
 
 
 
@@ -174,16 +212,16 @@ const std::string	Bureaucrat::GradeTooLowException::what
 
 // Canonical Four
 
-Bureaucrat::GradeTooLowException::GradeTooLowException	(void)	throw ():
-	std::domain_error (what)												{}
+Bureaucrat::GradeTooLowException::GradeTooLowException	(void)     throw ():
+	std::domain_error (what)                                                {}
 Bureaucrat::GradeTooLowException::GradeTooLowException
-(const GradeTooLowException	& original)							throw ():
-	std::domain_error (original)											{}
-Bureaucrat::GradeTooLowException	&
-Bureaucrat::GradeTooLowException::operator =
-(const GradeTooLowException	& other) throw ()
+	(const GradeTooLowException	& original)                        throw ():
+	std::domain_error (original)                                            {}
+Bureaucrat::GradeTooLowException
+	& Bureaucrat::GradeTooLowException::operator =
+	(const GradeTooLowException	& other)                           throw ()
 { std::domain_error::operator = (other); return *this; }
-Bureaucrat::GradeTooLowException::~ GradeTooLowException	(void) throw ()	{}
+Bureaucrat::GradeTooLowException::~ GradeTooLowException	(void) throw () {}
 
 
 
@@ -194,12 +232,12 @@ Bureaucrat::GradeTooLowException::~ GradeTooLowException	(void) throw ()	{}
 // Canonical Four
 
 // No default constructor, due to value requirement
-Bureaucrat::Grade::Grade
-(const Grade	& original) throw ():	g (original.g) {}
+Bureaucrat::Grade::Grade	(const Grade	& original)      throw ():
+	g (original.g)                                                    {}
 Bureaucrat::Grade
-& Bureaucrat::Grade::operator = (const Grade	& other) throw ()
+	& Bureaucrat::Grade::operator = (const Grade	& other) throw ()
 { g = other.g; return *this; }
-Bureaucrat::Grade::~ Grade	(void) throw () {}
+Bureaucrat::Grade::~ Grade	(void)                           throw () {}
 
 
 // other Constructors
@@ -210,25 +248,25 @@ Bureaucrat::Grade::Grade	(const unsigned char	& grade) throw ():
 
 // Operators
 
-Bureaucrat::Grade
-& Bureaucrat::Grade::operator ++ (void) throw () { -- g; return *this; }
-Bureaucrat::Grade	Bureaucrat::Grade::operator ++ (int) throw ()
+Bureaucrat::Grade	& Bureaucrat::Grade::operator ++ (void) throw ()
+{ -- g; return *this; }
+Bureaucrat::Grade	  Bureaucrat::Grade::operator ++ (int)  throw ()
 { Grade	before (*this); ++ *this; return before; }
-Bureaucrat::Grade
-& Bureaucrat::Grade::operator -- (void) throw () { ++ g; return *this; }
-Bureaucrat::Grade	Bureaucrat::Grade::operator -- (int) throw ()
+Bureaucrat::Grade	& Bureaucrat::Grade::operator -- (void) throw ()
+{ ++ g; return *this; }
+Bureaucrat::Grade	  Bureaucrat::Grade::operator -- (int)  throw ()
 { Grade	before (*this); -- *this; return before; }
 
-bool	Bureaucrat::Grade::operator < (const Grade	& other) const throw ()
-{ return g > other.g; }
-bool	Bureaucrat::Grade::operator > (const Grade	& other) const throw ()
-{ return other < *this; }
+bool	Bureaucrat::Grade::operator <  (const Grade	& other) const throw ()
+{ return g            >  other.g; }
+bool	Bureaucrat::Grade::operator >  (const Grade	& other) const throw ()
+{ return other        <  *this;   }
 bool	Bureaucrat::Grade::operator <= (const Grade	& other) const throw ()
-{ return not operator > (other); }
+{ return not operator >  (other); }
 bool	Bureaucrat::Grade::operator >= (const Grade	& other) const throw ()
-{ return not operator < (other); }
+{ return not operator <  (other); }
 bool	Bureaucrat::Grade::operator == (const Grade	& other) const throw ()
-{ return g == other.g; }
+{ return g            == other.g; }
 bool	Bureaucrat::Grade::operator != (const Grade	& other) const throw ()
 { return not operator == (other); }
 
@@ -244,8 +282,8 @@ Bureaucrat::Grade::operator unsigned short	(void) const throw () { return g; }
 // Utilities
 // ////
 
-std::string	ft_concatenate(
-		const std::string	& prefix, const unsigned char	& suffix) throw () {
+std::string	ft_concatenate
+		(const std::string	& prefix, const unsigned char	& suffix) throw () {
 	std::ostringstream	s;
 	s << prefix << static_cast <unsigned short>(suffix);
 	return s.str();
--- ex00/Bureaucrat.hpp
+++ ex00/Bureaucrat.hpp
@@ -6,11 +6,11 @@
 /*   By: lgasc <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 17:25:06 by lgasc             #+#    #+#             */
-/*   Updated: 2024/11/28 22:05:55 by lgasc            ###   ########.fr       */
+/*   Updated: 2025/02/12 18:14:38 by lgasc            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef BUREAUCRAT_HPP
+#ifndef  BUREAUCRAT_HPP
 # define BUREAUCRAT_HPP
 
 # include <ostream>
@@ -18,11 +18,14 @@
 # include <stdexcept> // domain_error
 
 # define WARN_UNUSED_RESULT	__attribute__ ((warn_unused_result))
-# define VIEW				const throw () WARN_UNUSED_RESULT
+# define VIEW              	const throw () WARN_UNUSED_RESULT
 
-# if __has_include ("Form.hpp")
+# if   __has_include ( "Form.hpp")
 
-class Form;
+class  Form;
+# elif __has_include ("AForm.hpp")
+
+class AForm;
 # endif
 
 
@@ -43,46 +46,45 @@ class Bureaucrat {
 	
 		static const std::string	what;
 	
-		GradeTooHighException
-			& operator = (const GradeTooHighException &) throw ();
+		      GradeTooHighException	& operator = (const t_self &) throw ();
 	public:
-		GradeTooHighException	(void)							throw ();
-		GradeTooHighException	(const GradeTooHighException &)	throw ();
-		~ GradeTooHighException	(void)							throw ();
+		      GradeTooHighException	             (        void  ) throw ();
+		      GradeTooHighException	             (const t_self &) throw ();
+		compl GradeTooHighException	             (        void  ) throw ();
 	};
 	class GradeTooLowException: public std::domain_error {
 		static const std::string	what;
 	
-		GradeTooLowException
-			& operator = (const GradeTooLowException &) throw ();
+		      GradeTooLowException	& operator =
+		                          	(const GradeTooLowException &) throw ();
 	public:
-		GradeTooLowException	(void)							throw ();
-		GradeTooLowException	(const GradeTooLowException &)	throw ();
-		~ GradeTooLowException	(void)							throw ();
+		      GradeTooLowException	(           void             ) throw ();
+		      GradeTooLowException	(const GradeTooLowException &) throw ();
+		compl GradeTooLowException	(           void             ) throw ();
 	};
 
 public:
 	class Grade {
 		unsigned char	g;
 	
-		Grade	(void)							throw ();
+		      Grade	             (      void   ) throw ();
 	public:
-		Grade	(const Grade &)					throw ();
-		Grade	& operator = (const Grade &)	throw ();
-		~ Grade	(void)							throw ();
+		      Grade	             (const Grade &) throw ();
+		      Grade	& operator = (const Grade &) throw ();
+		compl Grade	             (      void   ) throw ();
 	
 		explicit Grade	(const unsigned char & grade) throw ();
 	
-		Grade	& operator	++ (void)	throw (),
-				operator	++ (int)	throw () WARN_UNUSED_RESULT,
-				& operator	-- (void)	throw (),
-				operator	-- (int)	throw () WARN_UNUSED_RESULT;
-		bool	operator <	(const Grade &) VIEW,
-				operator >	(const Grade &) VIEW,
-				operator <=	(const Grade &) VIEW,
-				operator >=	(const Grade &) VIEW,
-				operator ==	(const Grade &) VIEW,
-				operator != (const Grade &) VIEW;
+		Grade	& operator ++ (void) throw (),
+		     	  operator ++ (int ) throw () WARN_UNUSED_RESULT,
+		     	& operator -- (void) throw (),
+		     	  operator -- (int ) throw () WARN_UNUSED_RESULT;
+		bool	operator <  (const Grade &) VIEW,
+		    	operator >  (const Grade &) VIEW,
+		    	operator <= (const Grade &) VIEW,
+		    	operator >= (const Grade &) VIEW,
+		    	operator == (const Grade &) VIEW,
+		    	operator != (const Grade &) VIEW;
 	
 		operator unsigned short	(void) const throw ();
 	};
@@ -97,45 +99,48 @@ public:
 
 private:
 	const std::string	name;
-	Grade				grade;
+	           Grade 	grade;
 
 
 	// Canonical Four
 
 public:
-	Bureaucrat		(void)							throw ();
-	Bureaucrat		(const Bureaucrat &)			throw ();
-	Bureaucrat	& operator = (const Bureaucrat &)	throw ();
-	~ Bureaucrat	(void)							throw ();
+	      Bureaucrat	             (      void        ) throw ();
+	      Bureaucrat	             (const Bureaucrat &) throw ();
+	      Bureaucrat	& operator = (const Bureaucrat &) throw ();
+	compl Bureaucrat	             (      void        ) throw ();
 
 
 	// other Constructors
 
 	explicit Bureaucrat
 		(const std::string & name, const unsigned char & grade = c_lowest_grade)
-		throw (GradeTooHighException, GradeTooLowException);
+			throw (GradeTooHighException, GradeTooLowException);
 
 
 	// Accessors
 
-	const std::string	& getName(void)		VIEW;
-	const Grade			& getGrade(void)	VIEW;
+	const std::string	& getName (void) VIEW;
+	const      Grade 	& getGrade(void) VIEW;
 
 
 	// Mutators
 
-	Bureaucrat	& operator	++ (void)	throw (GradeTooHighException),
-				operator	++ (int)	throw (GradeTooHighException)
-					WARN_UNUSED_RESULT,
-				& operator	-- (void)	throw (GradeTooLowException),
-				operator	-- (int)	throw (GradeTooLowException)
-					WARN_UNUSED_RESULT;
+	Bureaucrat	& operator ++ (void) throw (GradeTooHighException),
+	          	  operator ++ (int ) throw (GradeTooHighException)
+	          		WARN_UNUSED_RESULT,
+	          	& operator -- (void) throw (GradeTooLowException),
+	          	  operator -- (int ) throw (GradeTooLowException)
+	          		WARN_UNUSED_RESULT;
 
-# if __has_include ("Form.hpp")
 
 	// Methods
 
-	void	signForm(Form &) const throw ();
+# if   __has_include ( "Form.hpp")
+	void	signForm(Form  &) const throw ();
+# elif __has_include ("AForm.hpp")
+	void	signForm(AForm &) const throw ();
 # endif
+
 };
 #endif
